bee206705927acba2aaab9277e853809c602f05d
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-08-16 14:55:50 +0300

    Updated class Main

--- src/main/java/ru/babushkina/model/_main/Main.java
+++ src/main/java/ru/babushkina/model/_main/Main.java
@@ -1,132 +1,185 @@
 package ru.babushkina.model._main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Pattern;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "path/to/text_txt.txt";
+        File dataFolder = new File("data");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDate = dateFormat.format(new Date());
+        File currentFolder = new File(dataFolder, currentDate);
 
+        if (currentFolder.exists()) {
+            if (deleteDirectory(currentFolder)) {
+                System.out.println("Каталог \"" + currentDate + "\" успешно удален в папке \"data\".");
+            } else {
+                System.out.println("Ошибка при удалении каталога \"" + currentDate + "\" в папке \"data\".");
+                return;
+            }
+        }
+
+        boolean currentFolderCreated = currentFolder.mkdirs();
+        if (currentFolderCreated) {
+            System.out.println("Каталог \"" + currentDate + "\" успешно создан в папке \"data\".");
+        } else {
+            System.out.println("Ошибка при создании каталога \"" + currentDate + "\" в папке \"data\".");
+            return;
+        }
+
+        String filePath = "src/main/resources/text_txt.txt";
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            List<String> lines = Files.readAllLines(Path.of(filePath));
+            List<String> unknownWords = findUnknownWords(lines);
 
-            String firstUnknownWord = null;
-            String secondUnknownWord = null;
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] words = line.split("\\s+");
-
-                for (String word : words) {
-                    if (!isWord(word)) {
-                        if (firstUnknownWord == null) {
-                            firstUnknownWord = word;
-                        } else if (secondUnknownWord == null) {
-                            secondUnknownWord = word;
-                        }
-
-                        createFile(word, countVowels(word));
-                        break;
-                    }
-                }
-            }
-
-            reader.close();
-
-            if (firstUnknownWord != null) {
-                removeWordFromFile(firstUnknownWord, "path/to/even_vowels.txt");
-                removeWordFromFile(firstUnknownWord, "path/to/odd_vowels.txt");
-            }
-
-            if (secondUnknownWord != null) {
-                removeWordFromFile(secondUnknownWord, "path/to/even_vowels.txt");
-                removeWordFromFile(secondUnknownWord, "path/to/odd_vowels.txt");
+            if (unknownWords.size() >= 2) {
+                createFileInFolder(currentFolder, unknownWords.get(0));
+                createFileInFolder(currentFolder, unknownWords.get(1));
+                createPunctuationFileInFolder(currentFolder);
+                List<String> allWords = getAllWords(lines);
+                processFileInFolder(currentFolder, unknownWords.get(0), allWords, true);
+                processFileInFolder(currentFolder, unknownWords.get(1), allWords, false);
+                processPunctuationFileInFolder(currentFolder, lines);
+                System.out.println("Файлы успешно созданы и обработаны.");
+            } else {
+                System.out.println("В тексте недостаточно непонятных слов.");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static boolean isWord(String word) {
-        Pattern pattern = Pattern.compile("\\p{L}+");
-        return pattern.matcher(word).matches();
+    private static List<String> findUnknownWords(List<String> lines) {
+        List<String> unknownWords = new ArrayList<>();
+        for (String line : lines) {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                if (hasNumbers(word)) {
+                    unknownWords.add(word);
+                }
+            }
+        }
+        return unknownWords;
+    }
+
+    private static boolean hasNumbers(String word) {
+        for (char c : word.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void createFileInFolder(File folder, String fileName) throws IOException {
+        File file = new File(folder, fileName);
+        if (file.createNewFile()) {
+            FileWriter writer = new FileWriter(file);
+            writer.close();
+        } else {
+            System.out.println("Файл " + fileName + " уже существует в каталоге \"" + folder.getName() + "\".");
+        }
+    }
+
+    private static void createPunctuationFileInFolder(File folder) throws IOException {
+        File file = new File(folder, "punctuation");
+        if (file.createNewFile()) {
+            FileWriter writer = new FileWriter(file);
+            writer.close();
+        } else {
+            System.out.println("Файл punctuation уже существует в каталоге \"" + folder.getName() + "\".");
+        }
+    }
+
+    private static List<String> getAllWords(List<String> lines) {
+        List<String> allWords = new ArrayList<>();
+        for (String line : lines) {
+            BreakIterator iterator = BreakIterator.getWordInstance(Locale.US);
+            iterator.setText(line);
+            int start = iterator.first();
+
+            for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next()) {
+                String word = line.substring(start, end).trim().replaceAll("[^a-zA-Z]", "");
+                if (word.length() > 0) {
+                    allWords.add(word);
+                }
+            }
+        }
+        return allWords;
+    }
+
+    private static void processFileInFolder(File folder, String fileName, List<String> allWords, boolean isEvenVowels) throws IOException {
+        File file = new File(folder, fileName);
+        List<String> outputWords = new ArrayList<>();
+        for (String word : allWords) {
+            if (!hasNumbers(word)) {
+                int vowelsCount = countVowels(word);
+                if ((isEvenVowels && vowelsCount % 2 == 0) || (!isEvenVowels && vowelsCount % 2 != 0)) {
+                    outputWords.add(word);
+                }
+            }
+        }
+        FileWriter writer = new FileWriter(file);
+        writer.write(String.join(" ", outputWords));
+        writer.close();
     }
 
     private static int countVowels(String word) {
-        int count = 0;
         String vowels = "aeiouAEIOU";
-
+        int count = 0;
         for (char c : word.toCharArray()) {
             if (vowels.indexOf(c) != -1) {
                 count++;
             }
         }
-
         return count;
     }
 
-    private static void createFile(String fileName, int vowelCount) {
-        try {
-            FileWriter writer;
-            if (vowelCount % 2 == 0) {
-                writer = new FileWriter("path/to/even_vowels.txt", true);
-            } else {
-                writer = new FileWriter("path/to/odd_vowels.txt", true);
-            }
-
-            String cleanedWord = cleanWord(fileName);
-
-            Set<String> wordsSet = readWordsFromFile(writer);
-
-            if (!wordsSet.contains(cleanedWord)) {
-                writer.write(cleanedWord + " ");
-            }
-
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Set<String> readWordsFromFile(FileWriter writer) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(writer.toString()));
-        Set<String> wordsSet = new HashSet<>();
-        String line = reader.readLine();
-        reader.close();
-
-        if (line != null) {
-            line = cleanWord(line);
-            String[] words = line.trim().split("\\s+");
-            for (String word : words) {
-                wordsSet.add(word);
+    private static void processPunctuationFileInFolder(File folder, List<String> lines) throws IOException {
+        Map<Character, Integer> punctuationCounts = new HashMap<>();
+        for (String line : lines) {
+            for (char c : line.toCharArray()) {
+                if (isPunctuation(c)) {
+                    punctuationCounts.put(c, punctuationCounts.getOrDefault(c, 0) + 1);
+                }
             }
         }
-
-        return wordsSet;
-    }
-
-    private static String cleanWord(String word) {
-        return word.trim().replaceAll("[^\\p{L} ]", "");
-    }
-
-    private static void removeWordFromFile(String word, String fileName) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line = reader.readLine();
-            reader.close();
-
-            line = line.replaceAll("\\b" + word + "\\b", "");
-
-            FileWriter writer = new FileWriter(fileName);
-            writer.write(line);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        File file = new File(folder, "punctuation");
+        FileWriter writer = new FileWriter(file);
+        for (Map.Entry<Character, Integer> entry : punctuationCounts.entrySet()) {
+            writer.write(entry.getKey() + " " + entry.getValue() + "\n");
         }
+        writer.close();
+    }
+
+    private static boolean isPunctuation(char c) {
+        return !Character.isLetterOrDigit(c) && !Character.isWhitespace(c);
+    }
+
+    private static boolean deleteDirectory(File directory) {
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        deleteDirectory(file);
+                    } else {
+                        file.delete();
+                    }
+                }
+            }
+        }
+        return directory.delete();
     }
 }
