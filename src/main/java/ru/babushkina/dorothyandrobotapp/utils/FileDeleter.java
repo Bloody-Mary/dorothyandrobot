77c300ff189d0dce6a72e180721a8afec38fc306
Bloody-Mary <marianelalieben@gmail.com>
2023-08-19 10:34:49 +0300

    Created class FileDeleter and added method deleteDirectory

---
+++ src/main/java/ru/babushkina/dorothyandrobotapp/utils/FileDeleter.java
@@ -0,0 +1,21 @@
+package ru.babushkina.dorothyandrobotapp.utils;
+
+import java.io.File;
+
+public class FileDeleter {
+    public void deleteDirectory(File directory) {
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        deleteDirectory(file);
+                    } else {
+                        file.delete();
+                    }
+                }
+            }
+            directory.delete();
+        }
+    }
+}
