dade207624c04ae9343e1886ece641f5dbf3e07c
Bloody-Mary <marianelalieben@gmail.com>
2023-08-20 19:04:44 +0300

    Created class CustomFileReader and method readLines

---
+++ src/main/java/ru/babushkina/dorothyandrobotapp/model/CustomFileReader.java
@@ -0,0 +1,17 @@
+package ru.babushkina.dorothyandrobotapp.model;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class CustomFileReader {
+    public List<String> readLines(String filePath) {
+        try {
+            return Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+            return null;
+        }
+    }
+}
