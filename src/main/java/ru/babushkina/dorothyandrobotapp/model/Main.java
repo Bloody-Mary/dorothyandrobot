6e1a96cf82e4a6ff8a9df306a6612cd0bacc20e9
Bloody-Mary <marianelalieben@gmail.com>
2023-08-20 19:09:12 +0300

    Created class Main

---
+++ src/main/java/ru/babushkina/dorothyandrobotapp/model/Main.java
@@ -0,0 +1,28 @@
+package ru.babushkina.dorothyandrobotapp.model;
+
+import java.util.List;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomFileReader fileReader = new CustomFileReader();
+        List<String> lines = fileReader.readLines("src/main/resources/text_txt.txt");
+        WordProcessor wordProcessor = new WordProcessor();
+        List<String> unknownWords = wordProcessor.findUnknownWords(lines);
+        if (unknownWords.size() >= 2) {
+            CustomFileWriter customFileWriter = new CustomFileWriter();
+            String currentDate = customFileWriter.getCurrentDate();
+            File currentFolder = customFileWriter.createCurrentFolder("data", currentDate);
+            try {
+                customFileWriter.createFiles(currentFolder, unknownWords);
+                customFileWriter.createPunctuationFile(currentFolder);
+                List<String> allWords = wordProcessor.getAllWords(lines);
+                customFileWriter.processFiles(currentFolder, unknownWords, allWords);
+                customFileWriter.processPunctuationFile(currentFolder, lines);
+                System.out.println("Файлы успешно созданы и обработаны.");
+            } catch (IOException e) {
+                System.out.println("An error occurred during file creation or processing: " + e.getMessage());
+            }
+        }
+    }
+}
